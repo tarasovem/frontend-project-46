fbaacd7fac0fd362d4807f8fdc38d6c8d1162c16
Тарасов Евгений <tarasov@litres.ru>
2022-09-05 21:24:36 +0300

    Шаг 2. Добавляет аргументы (пути к обрабатываемым файлам) и опции (типы файлов)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,8 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
