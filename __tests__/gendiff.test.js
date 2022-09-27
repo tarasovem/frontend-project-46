f004ab7f13aa21756a9c8a000ac9df1406c0742b
Evgeniy Tarasov <tarasovem@gmail.com>
2022-09-28 00:18:15 +0300

    Шаг 5. Пишем тесты для будущей функциональности сравнения YAML-файлов

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,6 +8,14 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.resolve(__dirname, '../__fixtures__/', filename);
 const expectedResult = readFileSync(getFixturePath('expected-result.txt'), 'utf-8');
 
-test('Сравнение плоских JSON-файлов', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(expectedResult);
+describe('Сопоставление плоских данных в', () => {
+  it('JSON-файлах', () => {
+    expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(expectedResult);
+  });
+  it('YAML-файлах', () => {
+    expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toBe(expectedResult);
+    expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'))).toBe(expectedResult);
+    expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'))).toBe(expectedResult);
+    expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yml'))).toBe(expectedResult);
+  });
 });
